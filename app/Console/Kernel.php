c8a1bd29f45f29d4570c5783531864c97af1a2b2
Syed Ebad Ur Rehman <ebadshahid0123@gmail.com>
2026-08-13 12:07:52 +0500

    adjustt reminder schedular for development

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('reminders:send')->everyMinute();
+        // $schedule->command('reminders:send')->everyMinute();
+         // $schedule->command('reminders:send')->twiceDaily(0, 12);
+        $schedule->command('reminders:send');
     }
 
     /**
